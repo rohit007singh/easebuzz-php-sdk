b44a978302d398bc1853fb3a605ca7f2606f4434
rohit007singh <rohit.singh@brandzzy.com>
2024-11-13 13:00:38 +0530

    Add initial project structure with composer.json, .gitignore, and example payment handling scripts

---
+++ response.php
@@ -0,0 +1,38 @@
+<?php
+    // include file
+    // include_once('easebuzz-lib/easebuzz_payment_gateway.php');
+
+    require_once __DIR__ . '/src/easebuzz.php';
+
+    // salt for testing env
+    // get the slt from config.php file
+    $config = include('config.php');
+    $SALT = $config['SALT'];
+
+    /*
+    * Get the API response and verify response is correct or not.
+    *
+    * params string $easebuzzObj - holds the object of Easebuzz class.
+    * params array $_POST - holds the API response array.
+    * params string $SALT - holds the merchant salt key.
+    * params array $result - holds the API response array after valification of API response.
+    *
+    * ##Return values
+    *
+    * - return array $result - hoids API response after varification.
+    * 
+    * @params string $easebuzzObj - holds the object of Easebuzz class.
+    * @params array $_POST - holds the API response array.
+    * @params string $SALT - holds the merchant salt key.
+    * @params array $result - holds the API response array after valification of API response.
+    *
+    * @return array $result - hoids API response after varification.
+    *
+    */
+    $easebuzzObj = new Easebuzz($MERCHANT_KEY = null, $SALT, $ENV = null);
+    
+    $result = $easebuzzObj->easebuzzResponse( $_POST );
+ 
+    print_r($result);
+?>
+
---
+++ test.php
@@ -0,0 +1,41 @@
+<?php 
+
+require_once __DIR__ . '/src/easebuzz.php';
+
+$config = include('config.php');
+$MERCHANT_KEY = $config['MERCHANT_KEY'];
+$SALT = $config['SALT'];
+$ENV = $config['ENV'];
+
+$easebuzz = new Easebuzz($MERCHANT_KEY, $SALT, $ENV);
+$data = [
+    "txnid" => rand(100000,999999),
+    "amount" => "100.0",
+    "firstname" => "jitendra",
+    "email" => "test@gmail.com",
+    "phone" => "1231231235",
+    "productinfo" => "Laptop",
+    "surl" => "http://localhost:8000/response.php",
+    "furl" => "http://localhost:8000/response.php",
+    "udf1" => "",
+    "udf2" => "",
+    "udf3" => "",
+    "udf4" => "",
+    "udf5" => "",
+    "address1" => "",
+    "address2" => "",
+    "city" => "",
+    "state" => "",
+    "country" => "",
+    "zipcode" => ""
+];
+// return the status and the link
+$result = $easebuzz->initiatePaymentAPI($data);
+// redirect to the link
+easebuzzAPIResponse($result);
+
+function easebuzzAPIResponse($data){
+    header('Location: '.$data->data);
+}
+
+
